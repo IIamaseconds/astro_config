b289a0eada671237126ad42977e2ee4b57940b41
Kim M. Larsen <kml@duos.dk>
2023-03-30 23:37:38 +0200

    Sutmindutterino

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,12 @@
 return { -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
-}
+  Normal = { 
+    bgui = NONE,
+    fgui = NONE,
+    ctermbg = NONE,
+    ctermfb = NONE,
+    -- gui = NONE,
+
+  },
+
+} 
