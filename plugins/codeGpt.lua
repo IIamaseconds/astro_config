c1985812294833cb6a1d178971805e590ab86022
Mal <>
2023-05-14 15:52:38 +0200

    Disabled CodeGPT plugin

--- plugins/codeGpt.lua
+++ plugins/codeGpt.lua
@@ -1,19 +1,19 @@
 return {
-  "dpayne/CodeGPT.nvim",
-  -- http://neovimcraft.com/plugin/dpayne/CodeGPT.nvim/index.html
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
-  config = function()
-    require('codegpt.config');
-
-    vim.g["codegpt_openai_api_key"] = '<OpenAI Api Key>'
-    -- vim.g["codegpt_commands_defaults"] = {
-    --   ["test"] = {
-    --     user_message_template =
-    --     "write back the word 'test'"
-    --   }
-    -- }
-  end,
-  lazy = false
+--   "dpayne/CodeGPT.nvim",
+--   -- http://neovimcraft.com/plugin/dpayne/CodeGPT.nvim/index.html
+--   dependencies = {
+--     'nvim-lua/plenary.nvim',
+--   },
+--   config = function()
+--     require('codegpt.config');
+--
+--     vim.g["codegpt_openai_api_key"] = '<OpenAI Api Key>'
+--     -- vim.g["codegpt_commands_defaults"] = {
+--     --   ["test"] = {
+--     --     user_message_template =
+--     --     "write back the word 'test'"
+--     --   }
+--     -- }
+--   end,
+--   lazy = false
 }
