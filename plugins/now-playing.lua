406fe2fdf2e1528212270a73d88381283b0c92a7
Mal <kml@duos.dk>
2024-01-19 13:06:42 +0100

    Disable NowPlaying

--- plugins/now-playing.lua
+++ plugins/now-playing.lua
@@ -1,86 +1,95 @@
-local nowPlaying = {}
-local uv = vim.loop
-local timer = uv.new_timer()
-local previousTrack = "None"
+return {}
 
-nowPlaying.appleScript = [[
-  use AppleScript version "2.4"
-  use scripting additions
-
-  tell application "Spotify"
-    if player state is paused then
-      return "Spotify is paused"
-    else if player state is stopped then
-      return "Spotify is stopped"
-    else
-      set c to current track
-      set trackName to name of c
-      set artistName to artist of c
-      set albumName to album of c
-    end if
-  end tell
-
-  set trackInfo to artistName & " - " & trackName & linefeed & "Album: " & albumName
-  return trackInfo
-]]
-
-nowPlaying.onRead = function(err, chunk)
-  if err then
-    vim.notify("Error reading from stdout: " .. err, 1000, { title = "Now Playing" })
-    return
-  end
-
-  if chunk ~= nil then
-    local data = {}
-    if chunk then
-      table.insert(data, chunk)
-    end
-
-    local currentTrack = string.gsub(chunk, "\n$", "")
-
-    if currentTrack and currentTrack ~= previousTrack then
-      vim.notify(currentTrack, 1000, { title = "Now Playing" })
-      previousTrack = currentTrack
-    end
-  end
-end
-
-nowPlaying.start = function()
-  -- Execute the AppleScript using osascript
-  local stdout = uv.new_pipe(false)
-  uv.spawn('osascript', {
-    -- args = {'-e', nowPlaying.appleScript},
-    args = {'-e', nowPlaying.appleScript},
-    stdio = {nil, stdout},
-  }, function(code)
-
-    -- If the script ran successfully, read the output
-    if code == 0 and stdout then
-      stdout:read_stop()
-      stdout:close()
-    end
-  end)
-
-  if stdout then
-    -- Read the output of the AppleScript
-    uv.read_start(stdout, function(err, chunk)
-      nowPlaying.onRead(err, chunk)
-
-      -- Stop the timer if it's active
-      if timer and timer.is_active then
-        timer:stop()
-      end
-
-      -- Restart the timer
-      if timer then
-        uv.timer_start(timer, 1000, 5000, function()
-          nowPlaying.start()
-        end)
-      end
-    end)
-  end
-end
-
-return {
-  start = nowPlaying.start()
-}
+-- local nowPlaying = {}
+-- local uv = vim.loop
+-- local timer = uv.new_timer()
+-- local previousTrack = "None"
+--
+-- nowPlaying.appleScript = [[
+--   use AppleScript version "2.4"
+--   use scripting additions
+--
+--   tell application "System Events"
+--       set isSpotifyRunning to (count of (every process whose name is "Spotify")) > 0
+--   end tell
+--
+--   if isSpotifyRunning then
+--       tell application "Spotify"
+--           if player state is paused then
+--               return "Spotify is paused"
+--           else if player state is stopped then
+--               return "Spotify is stopped"
+--           else
+--               set c to current track
+--               set trackName to name of c
+--               set artistName to artist of c
+--               set albumName to album of c
+--           end if
+--       end tell
+--
+--       set trackInfo to artistName & " - " & trackName & linefeed & "[" & albumName & "]"
+--       return trackInfo
+--   else
+--       return "Not playing"
+--   end if
+-- ]]
+--
+-- nowPlaying.onRead = function(err, chunk)
+--   if err then
+--     vim.notify("Error reading from stdout: " .. err, 1000, { title = "Now Playing" })
+--     return
+--   end
+--
+--   if chunk ~= nil then
+--     local data = {}
+--     if chunk then
+--       table.insert(data, chunk)
+--     end
+--
+--     local currentTrack = string.gsub(chunk, "\n$", "")
+--
+--     if currentTrack and currentTrack ~= previousTrack then
+--       vim.notify(currentTrack, 1000, { title = "Now Playing" })
+--       previousTrack = currentTrack
+--     end
+--   end
+-- end
+--
+-- nowPlaying.start = function()
+--   -- Execute the AppleScript using osascript
+--   local stdout = uv.new_pipe(false)
+--   uv.spawn('osascript', {
+--     args = {'-e', nowPlaying.appleScript},
+--     stdio = {nil, stdout},
+--   }, function(code)
+--
+--     -- If the script ran successfully, read the output
+--     if code == 0 and stdout then
+--       stdout:read_stop()
+--       stdout:close()
+--     end
+--   end)
+--
+--   if stdout then
+--     -- Read the output of the AppleScript
+--     uv.read_start(stdout, function(err, chunk)
+--       nowPlaying.onRead(err, chunk)
+--
+--       -- Stop the timer if it's active
+--       if timer and timer.is_active then
+--         timer:stop()
+--       end
+--
+--       -- Restart the timer
+--       if timer then
+--         uv.timer_start(timer, 1000, 5000, function()
+--           nowPlaying.start()
+--         end)
+--       end
+--     end)
+--   end
+-- end
+--
+-- return {
+--   start = nowPlaying.start()
+-- }
