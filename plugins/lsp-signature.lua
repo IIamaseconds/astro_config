b96470972713daa0bc065090bcf3f81b6b25e1c2
Mal <kml@duos.dk>
2024-01-19 13:06:26 +0100

    Add LSP signature

---
+++ plugins/lsp-signature.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'ray-x/lsp_signature.nvim',
+    config = function()
+      require('lsp_signature').setup({
+        bind = true,
+        handler_opts = {
+          border = 'single',
+        },
+      })
+    end,
+    lazy = false,
+  }
+}
